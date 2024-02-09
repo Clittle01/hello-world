4148bfc12a55e092bc55c12d84a31a22278f5864
clittle0 <clittle0@frostburg.edu>
2024-02-09 18:25:10 -0500

    Updated Helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,7 @@ public class Helloworld {
     public static void main(String[] args) {
         
         System.out.println("Hello World!"); // This is the first line of code in Java!
+        System.out.println(("My name is Carlos Little and I major in computer science."));
     }
 
     
